f010aa6db89650b86b6267484eb314bbe78f7749
flaneuse <laura.d.hughes@gmail.com>
2017-03-17 16:42:11 -0400

    prelim data exploration

--- importPBS.R
+++ importPBS.R
@@ -8,8 +8,19 @@ dhs = read_stata("~/Downloads/GH_2014_DHS_03162017_1034_89151/ghkr72dt/GHKR72FL.
 
 # HW70 is the height/age std dev from new WHO
 
-pbs = read_stata('~/Downloads/12 - Ghana Data and Docs/M8_Anthropometry_For_ChildrenAll.dta')
+ch_raw = read_stata('~/Downloads/12 - Ghana Data and Docs/M8_Anthropometry_For_ChildrenAll.dta')
 
 
 hh = read_stata('~/Downloads/12 - Ghana Data and Docs/M1_HH_IdentificationAll.dta')
 hh = read_stata('~/Downloads/12 - Ghana Data and Docs/M1_final.dta')
+
+
+# rename vars for easier use ----------------------------------------------
+
+ch = ch_raw %>% 
+  mutate(ea = dense_rank(eacode)) %>% 
+  select(hhserial, ChildID, mid, CareGiver_ID, 
+         measured = q8_1, under5 = q8_2, age_months = q8_4,
+         sex = q8_5, weight = q8_6, height = q8_7, odema = q8_8,
+         dejure = q2_06a,
+         baseline, region = reg, district = dist, ea, eacode, datasource)
